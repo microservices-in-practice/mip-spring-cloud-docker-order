362345abb9ca2a30664ad4c679ace1811801cbfb
Krzysztof Sobkowiak <krzys.sobkowiak@gmail.com>
2019-08-22 16:15:47 +0200

    Hystrix enabled

--- src/main/java/com/capgemini/mip/order/client/customer/HystrixCustomerClient.java
+++ src/main/java/com/capgemini/mip/order/client/customer/HystrixCustomerClient.java
@@ -1,57 +1,61 @@
-package com.capgemini.mip.order.client.catalog;
+package com.capgemini.mip.order.client.customer;
 
 import com.capgemini.mip.order.config.ConfigProperties;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 @Service
 @Profile("hystrix")
-public class HystrixCatalogClient implements CatalogClient {
+public class HystrixCustomerClient implements CustomerClient {
 
   @Autowired
-  private RestTemplate restTemplate;
+  RestTemplate restTemplate;
 
   @Autowired
   private ConfigProperties properties;
 
-
   @Override
   @HystrixCommand(
-    fallbackMethod = "getAllItemsFallback",
+    fallbackMethod = "getAllCustomersFallback",
     commandProperties = {
       @HystrixProperty(
         name = "execution.isolation.thread.timeoutInMilliseconds",
         value = "500")
     })
-  public List<RemoteItemTO> getAllItems() {
-    return Arrays.asList(restTemplate.getForObject(properties.getCatalogServiceUrl(), RemoteItemTO[].class));
+  public List<RemoteCustomerTO> getAllCustomers() {
+    return restTemplate.exchange(properties.getCustomerServiceUrl(), HttpMethod.GET, null,
+      new ParameterizedTypeReference<List<RemoteCustomerTO>>() {
+      })
+      .getBody();
   }
 
-  public List<RemoteItemTO> getAllItemsFallback() {
+  public List<RemoteCustomerTO> getAllCustomersFallback() {
     return Collections.emptyList();
   }
 
   @Override
   @HystrixCommand(
-    fallbackMethod = "getItemByCodeFallback",
+    fallbackMethod = "getCustomerByCodeFallback",
     commandProperties = {
       @HystrixProperty(
         name = "execution.isolation.thread.timeoutInMilliseconds",
         value = "500")
     })
-  public RemoteItemTO getItemByCode(String code) {
-    return restTemplate.getForObject(properties.getCatalogServiceUrl() + code, RemoteItemTO.class);
+  public RemoteCustomerTO getCustomerByCode(String code) {
+    return restTemplate.getForObject(properties.getCustomerServiceUrl() + code, RemoteCustomerTO.class);
   }
 
-  public RemoteItemTO getItemByCodeFallback(String code) {
+  public RemoteCustomerTO getCustomerByCodeFallback(String code) {
     return null;
   }
+
 }
